9411ead535c1351f3ae9ebc3fb457c6ea2e13437
adilmahmud0113-commits <adilmahmud0113@gmail.com>
2026-09-05 10:15:21 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+function appendValue(val) {
+    document.getElementById('display').value += val;
+}
+
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
+
+function deleteLast() {
+    let current = document.getElementById('display').value;
+    document.getElementById('display').value = current.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (e) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
